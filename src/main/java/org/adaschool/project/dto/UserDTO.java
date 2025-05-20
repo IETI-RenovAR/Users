356b8daa9f111949a52f4382300f07f5faaee5af
johanestrada01 <johan.estrada-p@mail.escuelaing.edu.co>
2025-05-20 18:26:57 -0500

    roles

--- src/main/java/org/adaschool/project/dto/UserDTO.java
+++ src/main/java/org/adaschool/project/dto/UserDTO.java
@@ -1,19 +1,28 @@
 package org.adaschool.project.dto;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.adaschool.project.model.RoleEnum;
+
 public class UserDTO {
     private final String username;
     private final String email;
     private final String password;
+    private List<RoleEnum> roles;
 
     public UserDTO() {
         this.username = "";
         this.email = "";
+        this.roles = new ArrayList<>(Collections.singleton(RoleEnum.USER));
         this.password = "";
     }
 
     public UserDTO(String username, String email, String password) {
         this.username = username;
         this.email = email;
+        this.roles = new ArrayList<>(Collections.singleton(RoleEnum.USER));
         this.password = password;
     }
 
@@ -33,6 +42,16 @@ public class UserDTO {
 
     public String getPassword() {
         return password;
+    }
+
+        public List<RoleEnum> getRoles() {
+        return roles;
+    }
+
+    public void addRole(RoleEnum role) {
+        if (!roles.contains(role)) {
+            roles.add(role);
+        }
     }
 
 }
